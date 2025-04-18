4c7e9004e03695540e873276b3f0b5fae487b6f5
Reza Soltani <soltanireza65@gmail.com>
2025-04-18 18:15:54 +0330

    init code modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DoctorModule } from './doctor/doctor.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DoctorModule, BookingModule],
 })
 export class AppModule {}
