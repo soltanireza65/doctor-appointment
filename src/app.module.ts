34ccff647bceb2b2ab8aa37d75322321d0c3e0db
Reza Soltani <soltanireza65@gmail.com>
2025-04-18 18:21:06 +0330

    import config module and set schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DoctorModule } from './doctor/doctor.module';
 import { BookingModule } from './booking/booking.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DoctorModule, BookingModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+    DoctorModule,
+    BookingModule,
+  ],
 })
 export class AppModule {}
